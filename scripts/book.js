d1a004dfcb2cab7991ccda45fabf3f28e874ef88
Francis Langit <francis.villanueva.langit@gmail.com>
2021-03-31 17:06:58 +0800

    Create display function for Book objects
    Properties of Book objects can now be displayed in Bootstrap cards.

--- scripts/book.js
+++ scripts/book.js
@@ -1,12 +1,12 @@
 export default class Book {
     /**Represents a book in the user's library. Properties hold data related
      * to such. To be stored in a Library object's books attribute array.*/
-    constructor(title, author, linkToCoverPhoto, pages, 
+    constructor(linkToCoverPhoto, title, author, pages, 
         publisher, publicationYear, isbn, language) {
             this.read = false;
+            this.linkToCoverPhoto = linkToCoverPhoto;
             this.title = title;
             this.author = author;
-            this.linkToCoverPhoto = linkToCoverPhoto;
             this.pages = pages;
             this.publisher = publisher;
             this.publicationYear =  publicationYear;
@@ -15,8 +15,19 @@ export default class Book {
     }    
 
     display() {
-        const book = document.querySelector('.book');
-        const node = document.importNode(book.content, true);
-        document.body.appendChild(node);
+        const bookTemplate = document.querySelector('.book');
+        const book = document.importNode(bookTemplate.content, true);
+        
+        book.querySelector("#cover").src = this.linkToCoverPhoto;
+
+        book.querySelector("#title").innerHTML += this.title;
+        book.querySelector("#author").innerHTML += this.author;
+        book.querySelector("#pages").innerHTML += this.pages;
+        book.querySelector("#publisher").innerHTML += this.publisher;
+        book.querySelector("#publicationYear").innerHTML += this.publicationYear;
+        book.querySelector("#isbn").innerHTML += this.isbn;
+        book.querySelector("#language").innerHTML += this.language;
+
+        document.body.appendChild(book);
     }
 }
--- scripts/main.js
+++ scripts/main.js
@@ -22,9 +22,9 @@ class Library {
 let library = new Library;
 
 let book1 = new Book(
+    'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1398034300l/5107.jpg',
     'Catcher in the Rye', 
     'J.D. Salinger', 
-    'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1398034300l/5107.jpg',
     '277',
     'Little, Brown and Company',
     '1951',
@@ -32,6 +32,18 @@ let book1 = new Book(
     'English',
 );
 
+let book2 = new Book(
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/To_Kill_a_Mockingbird_%28first_edition_cover%29.jpg/220px-To_Kill_a_Mockingbird_%28first_edition_cover%29.jpg',
+    'To Kill a Mockingbird', 
+    'Harper Lee', 
+    '281',
+    'J. B. Lippincott & Co.',
+    '1960',
+    '0446310786',
+    'English',
+);
+
 library.addBook(book1);
+library.addBook(book2);
 
 library.displayLibrary();
