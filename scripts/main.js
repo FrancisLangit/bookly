1eb613f012e80424db00dd80bd70a82b091b55ef
Francis Langit <francis.villanueva.langit@gmail.com>
2021-03-31 14:21:40 +0800

    Create Library class
    addBook() and displayBooks() methods were created within it as well.

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,40 @@
 class Book {
-    constructor(title, author, linkToCoverPhoto, pages, publisher,
-        publicationYear, isbn, language) {
-        this.read = false;
-
-        // Required attributes. 
-        this.title = title;
-        this.author = author;
-
-        // Optional attributes.
-        this.linkToCoverPhoto = linkToCoverPhoto;
-        this.pages = pages;
-        this.publisher = publisher;
-        this.publicationYear =  publicationYear;
-        this.language = language;
+    constructor(title, author, linkToCoverPhoto, pages, 
+        publisher, publicationYear, isbn, language) {
+            this.read = false;
+            this.title = title;
+            this.author = author;
+            this.linkToCoverPhoto = linkToCoverPhoto;
+            this.pages = pages;
+            this.publisher = publisher;
+            this.publicationYear =  publicationYear;
+            this.isbn = isbn;
+            this.language = language;
     }    
 }
 
-class Main {
+class Library {
     constructor() {
-        this.library = [];
+        this.books = [];
+    }
+
+    addBook(book) {
+        this.books.push(book);
+    }
+
+    displayBooks() {
+        for (let i = 0; i < this.books.length; i++) {
+            console.log(this.books[i].title, this.books[i].author);
+        }
     }
 }
+
+library = new Library;
+
+book1 = new Book("catcher in the rye", "j.d. salinger", null, 500);
+book2 = new Book("harry potter 1", "jk rowling", null, 234);
+
+library.addBook(book1);
+library.addBook(book2);
+
+library.displayBooks();
