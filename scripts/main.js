69b34daf09805b245e271c11e0a0af3eb84a15ec
Francis Langit <francis.villanueva.langit@gmail.com>
2021-03-31 14:28:04 +0800

    Add docstrings to classes and methods

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,6 @@
 class Book {
+    /**Represents a book in the user's library. Attributes hold data related
+     * to such. To be stored in a Library object's books attribute array.*/
     constructor(title, author, linkToCoverPhoto, pages, 
         publisher, publicationYear, isbn, language) {
             this.read = false;
@@ -14,15 +16,18 @@ class Book {
 }
 
 class Library {
+    /**Represents a user's library. Holds user's Book objects.*/
     constructor() {
         this.books = [];
     }
 
     addBook(book) {
+        /**Adds a book to the user's library.*/
         this.books.push(book);
     }
 
     displayBooks() {
+        /**Displays all books in the user's library.*/
         for (let i = 0; i < this.books.length; i++) {
             console.log(this.books[i].title, this.books[i].author);
         }
