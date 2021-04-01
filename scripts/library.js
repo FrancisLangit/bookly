efc6023a44f4c8f49e5780b8946a8e10a4324ded
Francis Langit <francis.villanueva.langit@gmail.com>
2021-04-01 13:37:48 +0800

    Place Library class into separate file

---
+++ scripts/library.js
@@ -0,0 +1,18 @@
+export default class Library {
+    /**Represents a user's library. Holds user's Book objects.*/
+    constructor() {
+        this.books = [];
+    }
+
+    addBook(book) {
+        /**Adds a book to the user's library.*/
+        this.books.push(book);
+    }
+
+    displayLibrary() {
+        /**Displays all books in the user's library.*/
+        for (let i = 0; i < this.books.length; i++) {
+            this.books[i].display();
+        }
+    }
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,6 @@
-import Book from './book.js'
+import Book from './book.js';
+import Library from './library.js';
 
-class Library {
-    /**Represents a user's library. Holds user's Book objects.*/
-    constructor() {
-        this.books = [];
-    }
-
-    addBook(book) {
-        /**Adds a book to the user's library.*/
-        this.books.push(book);
-    }
-
-    displayLibrary() {
-        /**Displays all books in the user's library.*/
-        for (let i = 0; i < this.books.length; i++) {
-            this.books[i].display();
-        }
-    }
-}
 
 let library = new Library;
 
