48f6631ae5b6151011f13fe1d9e46636be5c8f29
Francis Langit <francis.villanueva.langit@gmail.com>
2021-04-01 13:35:52 +0800

    Refactor book class
    - Made createBookCard() and fillBookCard() methods.
    - Added docstrings to all methods without such.

--- scripts/book.js
+++ scripts/book.js
@@ -1,33 +1,49 @@
 export default class Book {
     /**Represents a book in the user's library. Properties hold data related
-     * to such. To be stored in a Library object's books attribute array.*/
-    constructor(linkToCoverPhoto, title, author, pages, 
+     * to such. To be stored in a Library object's books property array.*/
+    constructor(linkToCoverPhoto, title, author, pages,
         publisher, publicationYear, isbn, language) {
-            this.read = false;
-            this.linkToCoverPhoto = linkToCoverPhoto;
-            this.title = title;
-            this.author = author;
-            this.pages = pages;
-            this.publisher = publisher;
-            this.publicationYear =  publicationYear;
-            this.isbn = isbn;
-            this.language = language;
-    }    
+        this.linkToCoverPhoto = linkToCoverPhoto;
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.publisher = publisher;
+        this.publicationYear = publicationYear;
+        this.isbn = isbn;
+        this.language = language;
+        this.read = false;
+    }
+
+    createBookCard() {
+        /**Grabs .book template from index.html and returns such as a node.*/
+        const bookCardTemplate = document.querySelector('.book');
+        return document.importNode(bookCardTemplate.content, true);
+    }
+
+    fillBookCard(bookCard) {
+        /**Gets index.html .book template as argument and fills such up with 
+         * the Book object's data.*/
+        bookCard.querySelector('#bookCover').src = (this.linkToCoverPhoto);
+        bookCard.querySelector('#bookTitle').innerHTML = (this.title);
+        bookCard.querySelector('#bookAuthor').innerHTML = (
+            `by ${this.author}`);
+        bookCard.querySelector('#bookPages').innerHTML = (
+            `Pages: ${this.pages}`);
+        bookCard.querySelector('#bookPublisher').innerHTML = (
+            `Publisher: ${this.publisher}`);
+        bookCard.querySelector('#bookPublicationYear').innerHTML = (
+            `Publication Year: ${this.publicationYear}`);
+        bookCard.querySelector('#bookIsbn').innerHTML = (
+            `ISBN: ${this.isbn}`);
+        bookCard.querySelector('#bookLanguage').innerHTML = (
+            `Language: ${this.language}`);
+    }
 
     display() {
-        const bookTemplate = document.querySelector('.book');
-        const book = document.importNode(bookTemplate.content, true);
-        
-        book.querySelector("#cover").src = this.linkToCoverPhoto;
-
-        book.querySelector("#title").innerHTML += this.title;
-        book.querySelector("#author").innerHTML += this.author;
-        book.querySelector("#pages").innerHTML += this.pages;
-        book.querySelector("#publisher").innerHTML += this.publisher;
-        book.querySelector("#publicationYear").innerHTML += this.publicationYear;
-        book.querySelector("#isbn").innerHTML += this.isbn;
-        book.querySelector("#language").innerHTML += this.language;
-
-        document.body.appendChild(book);
+        /**Creates a book card and fills it up with Book object's data. Then 
+         * appends such to the webpage.*/
+        const bookCard = this.createBookCard();
+        this.fillBookCard(bookCard)
+        document.body.appendChild(bookCard);
     }
 }
