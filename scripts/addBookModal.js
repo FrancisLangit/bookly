faea61c68f45d89def1663ec4b21b3b04a86390a
Francis Langit <francis.villanueva.langit@gmail.com>
2021-04-02 15:39:58 +0800

    Add functionality to add book modal cancel button
    Also refactored AddBookModal class in the process.

---
+++ scripts/addBookModal.js
@@ -0,0 +1,46 @@
+import Book from './book.js';
+
+export default class AddBookModal {
+    /**Represents modal that pops up when user clicks "Add Book" card.*/
+    constructor(main) {
+        this.main = main;
+    }
+
+    clearForm() {
+        /**Clears out title, author, and pages inputs in modal form.*/
+        document.querySelector('#inputBookTitle').value = "";
+        document.querySelector('#inputBookAuthor').value = "";
+        document.querySelector('#inputBookPages').value = "";
+    }
+
+    setUpCancelButton() {
+        /**Adds click event listener to modal cancel button that makes it 
+         * clear the form before closing the modal.*/
+        this.cancelButton = document.querySelector(
+            '#addBookModalCancelButton');        
+        this.cancelButton.addEventListener('click', this.clearForm);
+    }
+
+    setUpSaveButton() {
+        /**Adds click event listener to modal save button that makes it create
+         * and display a new Book object, add such to the user's library, and 
+         * clear the modal form before closing it.*/
+        const saveButton = document.querySelector('#addBookModalSaveButton');
+        saveButton.addEventListener('click', () => {
+            const newBook = new Book(
+                document.querySelector('#inputBookTitle').value,
+                document.querySelector('#inputBookAuthor').value,
+                document.querySelector('#inputBookPages').value
+            ); 
+            newBook.display();
+            this.main.library.addBook(newBook);
+            this.clearForm();
+        });
+    }
+
+    setUp() {
+        /**Sets up all the functionalities of the modal.*/
+        this.setUpCancelButton();
+        this.setUpSaveButton();
+    }
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,15 @@
-import Book from './book.js';
+import AddBookModal from './addBookModal.js';
 import Library from './library.js';
 
-class AddBookModal {
-    constructor(main) {
-        this.main = main;
-    }
-
-    setUp() {
-        const saveButton = document.querySelector('#addBookModalSaveButton');
-        saveButton.addEventListener('click', () => {
-            const title = document.querySelector('#inputBookTitle').value;
-            const author = document.querySelector('#inputBookAuthor').value;
-            const pages = document.querySelector('#inputBookPages').value;
-            const newBook = new Book(title, author, pages); 
-            newBook.display();
-            this.main.library.addBook(newBook);
-        });
-    }
-}
-
 class Main {
+    /**Holds logic of the website's JavaScript together.*/
     constructor() {
         this.library = new Library;
         this.addBookModal = new AddBookModal(this);
     }
 
     setUp() {
+        /**Sets up the user interface.*/
         this.addBookModal.setUp();
         this.library.display();
     }
