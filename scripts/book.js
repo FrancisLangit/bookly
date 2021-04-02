033a9a5ce8a5c58a1f6536dc20b3f5d60795c079
Francis Langit <francis.villanueva.langit@gmail.com>
2021-04-02 13:57:00 +0800

    Remove miscellaneous properties of Book class
    Other than these properties:
        - this.title
        - this.author
        - this.pages
        - this.read
    
    All others have been removed from constructor of Book class.

--- scripts/book.js
+++ scripts/book.js
@@ -1,17 +1,11 @@
 export default class Book {
     /**Represents a book in the user's library. Properties hold data related
      * to such. To be stored in a Library object's books property array.*/
-    constructor(linkToCoverPhoto, title, author, pages,
-        publisher, publicationYear, isbn, language) {
-        this.linkToCoverPhoto = linkToCoverPhoto;
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.publisher = publisher;
-        this.publicationYear = publicationYear;
-        this.isbn = isbn;
-        this.language = language;
-        this.read = false;
+    constructor(title, author, pages) {
+            this.title = title;
+            this.author = author;
+            this.pages = pages;
+            this.read = false;
     }
 
     createBookCard() {
@@ -23,20 +17,11 @@ export default class Book {
     fillBookCard(bookCard) {
         /**Gets index.html .book template as argument and fills such up with 
          * the Book object's data.*/
-        bookCard.querySelector('#bookCover').src = (this.linkToCoverPhoto);
         bookCard.querySelector('#bookTitle').innerHTML = (this.title);
         bookCard.querySelector('#bookAuthor').innerHTML = (
             `by ${this.author}`);
         bookCard.querySelector('#bookPages').innerHTML = (
-            `Pages: ${this.pages}`);
-        bookCard.querySelector('#bookPublisher').innerHTML = (
-            `Publisher: ${this.publisher}`);
-        bookCard.querySelector('#bookPublicationYear').innerHTML = (
-            `Publication Year: ${this.publicationYear}`);
-        bookCard.querySelector('#bookIsbn').innerHTML = (
-            `ISBN: ${this.isbn}`);
-        bookCard.querySelector('#bookLanguage').innerHTML = (
-            `Language: ${this.language}`);
+            `${this.pages} Pages`);
     }
 
     display() {
--- scripts/main.js
+++ scripts/main.js
@@ -5,80 +5,45 @@ import Library from './library.js';
 let library = new Library;
 
 let book1 = new Book(
-    'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1398034300l/5107.jpg',
     'Catcher in the Rye', 
     'J.D. Salinger', 
     '277',
-    'Little, Brown and Company',
-    '1951',
-    '0316769177',
-    'English',
 );
 
 let book2 = new Book(
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/To_Kill_a_Mockingbird_%28first_edition_cover%29.jpg/220px-To_Kill_a_Mockingbird_%28first_edition_cover%29.jpg',
     'To Kill a Mockingbird', 
     'Harper Lee', 
     '281',
-    'J. B. Lippincott & Co.',
-    '1960',
-    '0446310786',
-    'English',
 );
 
 let book3 = new Book(
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/To_Kill_a_Mockingbird_%28first_edition_cover%29.jpg/220px-To_Kill_a_Mockingbird_%28first_edition_cover%29.jpg',
     'To Kill a Mockingbird', 
     'Harper Lee', 
     '281',
-    'J. B. Lippincott & Co.',
-    '1960',
-    '0446310786',
-    'English',
 );
 
 let book4 = new Book(
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/To_Kill_a_Mockingbird_%28first_edition_cover%29.jpg/220px-To_Kill_a_Mockingbird_%28first_edition_cover%29.jpg',
     'To Kill a Mockingbird', 
     'Harper Lee', 
     '281',
-    'J. B. Lippincott & Co.',
-    '1960',
-    '0446310786',
-    'English',
 );
 
 let book5 = new Book(
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/To_Kill_a_Mockingbird_%28first_edition_cover%29.jpg/220px-To_Kill_a_Mockingbird_%28first_edition_cover%29.jpg',
     'To Kill a Mockingbird', 
     'Harper Lee', 
     '281',
-    'J. B. Lippincott & Co.',
-    '1960',
-    '0446310786',
-    'English',
 );
 
 let book6 = new Book(
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/To_Kill_a_Mockingbird_%28first_edition_cover%29.jpg/220px-To_Kill_a_Mockingbird_%28first_edition_cover%29.jpg',
     'To Kill a Mockingbird', 
     'Harper Lee', 
     '281',
-    'J. B. Lippincott & Co.',
-    '1960',
-    '0446310786',
-    'English',
 );
 
 let book7 = new Book(
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/To_Kill_a_Mockingbird_%28first_edition_cover%29.jpg/220px-To_Kill_a_Mockingbird_%28first_edition_cover%29.jpg',
     'To Kill a Mockingbird', 
     'Harper Lee', 
     '281',
-    'J. B. Lippincott & Co.',
-    '1960',
-    '0446310786',
-    'English',
 );
 
 library.addBook(book1);
