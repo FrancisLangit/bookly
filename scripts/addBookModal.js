62e30c5b5566c86a113e9e72a9e11e33a7c23855
Francis Langit <francis.villanueva.langit@gmail.com>
2021-04-03 16:34:36 +0800

    Add input validation to Add Book modal form
    Pressing save if one or more input fields empty will not close modal.

--- scripts/addBookModal.js
+++ scripts/addBookModal.js
@@ -4,13 +4,25 @@ export default class AddBookModal {
     /**Represents modal that pops up when user clicks "Add Book" card.*/
     constructor(main) {
         this.main = main;
+        this.modal = new bootstrap.Modal(
+            document.querySelector('#addBookModal'));
+    }
+
+    getInputValues() {
+        /**Returns list of the modal's form input's values.*/
+        return [
+            document.querySelector('#inputBookTitle').value,
+            document.querySelector('#inputBookAuthor').value,
+            document.querySelector('#inputBookPages').value,
+        ];
     }
 
     clearForm() {
         /**Clears out title, author, and pages inputs in modal form.*/
-        document.querySelector('#inputBookTitle').value = "";
-        document.querySelector('#inputBookAuthor').value = "";
-        document.querySelector('#inputBookPages').value = "";
+        const inputValues = this.getInputValues();
+        for (let i = 0; i < getInputValues.length; i++) {
+            inputValues[i] = '';
+        } 
         document.querySelector('#inputBookReadStatus').checked = false;
     }
 
@@ -22,21 +34,35 @@ export default class AddBookModal {
         this.cancelButton.addEventListener('click', this.clearForm);
     }
 
+    isInputValid() {
+        /**Returns true if none of the form input values are left blank.*/
+        const inputValues = this.getInputValues();
+        return (!inputValues.includes(''));
+    }
+
+    createBookObject() {
+        /**Creates a new Book object based on values of modal form inputs.*/
+        return new Book(
+            document.querySelector('#inputBookTitle').value,
+            document.querySelector('#inputBookAuthor').value,
+            document.querySelector('#inputBookPages').value,
+            document.querySelector('#inputBookReadStatus').checked,
+        );
+    }
+
     setUpSaveButton() {
         /**Adds click event listener to modal save button that makes it create
          * and display a new Book object, add such to the user's library, and 
-         * clear the modal form before closing it.*/
+         * clears the modal form after closing it.*/
         const saveButton = document.querySelector('#addBookModalSaveButton');
         saveButton.addEventListener('click', () => {
-            const newBook = new Book(
-                document.querySelector('#inputBookTitle').value,
-                document.querySelector('#inputBookAuthor').value,
-                document.querySelector('#inputBookPages').value,
-                document.querySelector('#inputBookReadStatus').checked,
-            ); 
-            newBook.display();
-            this.main.library.addBook(newBook);
-            this.clearForm();
+            if (this.isInputValid()) {
+                const newBook = this.createBookObject(); 
+                newBook.display();
+                this.main.library.addBook(newBook);
+                this.modal.hide();
+                this.clearForm();
+            }
         });
     }
 
