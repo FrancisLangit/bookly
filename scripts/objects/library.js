82e40336991004d692b4624ace95388a41a67c2b
Francis Langit <francis.villanueva.langit@gmail.com>
2021-04-10 09:55:03 +0800

    Create Library.removeBookFromLocalStorage()
    Method removes book passed as argument from user's local storage.

--- scripts/objects/library.js
+++ scripts/objects/library.js
@@ -2,8 +2,17 @@ export default class Library {
     /**Represents a user's library. Holds and displays user's Book objects.*/
     constructor(main) {
         this.main = main;
-        this.auth = main.auth;
         this.books = [];
+    }
+
+    changeDisplayIfEmpty() {
+        /**Displays #emptyLibraryText div if user has no books in library.*/
+        const emptyLibraryText = document.querySelector('#emptyLibraryText');
+        if (this.books.length <= 0) {
+            emptyLibraryText.hidden = false;
+        } else {
+            emptyLibraryText.hidden = true;
+        }
     }
 
     addBookToLocalStorage(book) {
@@ -26,28 +35,30 @@ export default class Library {
         this.changeDisplayIfEmpty();
     }
 
+    removeBookFromLocalStorage(bookToDelete) {
+        /**Removes a book from the user's locally stored booklyLibrary 
+         * array.*/
+        this.main.localLibrary = this.main.localLibrary.filter(
+            function(book) {
+                const bookObj = JSON.parse(book);
+                return bookObj.id !== bookToDelete.id;
+            }
+        );
+        this.main.saveLocalStorage();
+    }
+
     removeBook(bookToDelete) {
         /**Takes a Book object as an argument and removes such from 
          * Library.books.*/
+        this.removeBookFromLocalStorage(bookToDelete);
         this.books = this.books.filter(function(book) {
             return book.id !== bookToDelete.id;
         });
         this.changeDisplayIfEmpty();
     }
 
-    changeDisplayIfEmpty() {
-        /**Displays #emptyLibraryText div if user has no books in library.*/
-        const emptyLibraryText = document.querySelector('#emptyLibraryText');
-        if (this.books.length <= 0) {
-            emptyLibraryText.hidden = false;
-        } else {
-            emptyLibraryText.hidden = true;
-        }
-    }
-
     display() {
         /**Displays all books in the user's library.*/
-        // this.changeDisplayIfUserSignedin();
         this.changeDisplayIfEmpty();
         for (let i = 0; i < this.books.length; i++) {
             this.books[i].display();
